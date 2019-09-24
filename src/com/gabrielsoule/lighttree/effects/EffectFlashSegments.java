e84d64eac80f3a5a8f069ce8140e76fd65950e14
Gabriel Soule <gabesoule@gmail.com>
2019-09-24 16:04:08 -0700

    Implement first (likely broken) iteration of multiple color gradient

--- src/com/gabrielsoule/lighttree/effects/EffectFlashSegments.java
+++ src/com/gabrielsoule/lighttree/effects/EffectFlashSegments.java
@@ -1,20 +1,27 @@
 package com.gabrielsoule.lighttree.effects;
 
+import com.gabrielsoule.lighttree.ColorGradient;
 import com.gabrielsoule.lighttree.LightEffect;
+import com.gabrielsoule.lighttree.LightTree;
+
+import java.util.*;
 
 public class EffectFlashSegments extends LightEffect {
 
     //change this based on actual hardware setup
     private final int LIGHTS_PER_SEGMENT = 64;
+    private ColorGradient color;
 
     //we scramble this array during setup so that segments turn on randomly
-    private final int[] segments = new int[p.NUM_LIGHTS / LIGHTS_PER_SEGMENT];
+    private final Deque<Integer> segments = new ArrayDeque<>();
+    public EffectFlashSegments(LightTree p, int startColor, int endColor) {
+        for(int i = 0; i < p.NUM_LIGHTS / LIGHTS_PER_SEGMENT; i++) {
+            segments.addFirst(i * p.NUM_LIGHTS);
+        }
+    }
 
     @Override
     public void setup() {
-        for(int i = 0; i < segments.length; i++) {
-            segments[i] = LIGHTS_PER_SEGMENT * i;
-        }
     }
 
     @Override
