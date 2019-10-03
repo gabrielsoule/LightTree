f59a2a6572e2d363b6d0131e80bca9f295dd3874
gabrielsoule <gabesoule@gmail.com>
2019-10-03 12:53:29 -0700

    work on pulsers

--- src/com/gabrielsoule/lighttree/Config.java
+++ src/com/gabrielsoule/lighttree/Config.java
@@ -1,5 +1,21 @@
 package com.gabrielsoule.lighttree;
 
-public class Config {
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Config {
+    public Config(String filePath) {
+        Yaml yaml = new Yaml();
+        try {
+            yaml.load(new FileInputStream(new File(filePath)));
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to load configuration file " + filePath);
+            System.exit(-1);
+        }
+
+        
+    }
 }
