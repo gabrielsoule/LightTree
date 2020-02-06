b4c19f686fa0fb4e21fdcada723f1b7e959a2a08
Gabriel Soule <gabesoule@gmail.com>
2020-02-06 09:08:57 -0800

    implemented gradient boxes; worked a little on colors

---
+++ src/main/java/com/gabrielsoule/lighttree/effect/EffectFlicker.java
@@ -0,0 +1,15 @@
+package com.gabrielsoule.lighttree.effect;
+
+import com.gabrielsoule.lighttree.LightEffect;
+
+public class EffectFlicker extends LightEffect {
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
